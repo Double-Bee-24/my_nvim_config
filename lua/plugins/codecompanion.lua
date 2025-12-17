b58aa7323849b2d3d01913a91842f6cfceb40c60
Double-Bee-24 <bodya24102001@gmail.com>
2025-12-17 20:35:35 +0200

    updated plugins

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -1,31 +1,29 @@
 return {
-  'olimorris/codecompanion.nvim',
-  dependencies = { 'nvim-lua/plenary.nvim' },
-  opts = {
-    strategies = {
-      chat = {
-        adapter = 'anthropic',
-        model = 'glm-4.6',
-      },
-      inline = {
-        adapter = 'anthropic',
-        model = 'glm-4.6',
-      },
-    },
+  {
+    'olimorris/codecompanion.nvim',
+    version = '^18.0.0',
     opts = {
-      log_level = 'DEBUG',
-    },
-    adapters = {
-      http = {
-        anthropic = function()
-          return require('codecompanion.adapters').extend('anthropic', {
-            env = {
-              base_url = 'https://api.z.ai/api/anthropic',
-              api_key = 'c8335f96ec4e4402ad493ac36c805ddf.WkIfjqqZL5x1boHi',
-            },
-          })
-        end,
+      adapters = {
+        gemini = {
+          type = 'gemini',
+          env = {
+            api_key = 'GEMINI_API_KEY',
+          },
+          model = 'models/gemini-1.5-pro',
+        },
       },
+      strategies = {
+        chat = {
+          adapter = 'gemini',
+        },
+        inline = {
+          adapter = 'gemini',
+        },
+      },
+    },
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-treesitter/nvim-treesitter',
     },
   },
 }
